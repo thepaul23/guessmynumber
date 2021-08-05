a8f6a660a00c00b6388c27865ec8d290fcb4bd40
Catalin <catalin@proportunity.co>
2021-08-05 12:42:49 +0300

    removed unnecessary comments

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/*
-console.log(document.querySelector(`.message`).textContent);
-document.querySelector(`.message`).textContent = `Correct Number!🎉`;
-
-document.querySelector(`.number`).textContent = 13;
-document.querySelector(`.score`).textContent = 20;
-
-document.querySelector(`.guess`).value = 23;
-console.log(document.querySelector(`.guess`).value);
-*/
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
@@ -41,26 +31,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.message`).textContent = `you lost the game 😞`;
       document.querySelector(`.score`).textContent = 0;
     }
-  } // else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector(`.message`).textContent = `📉 Too high`;
-  //     score--;
-  //     document.querySelector(`.score`).textContent = score;
-  //   } else {
-  //     document.querySelector(`.message`).textContent = `you lost the game 😞`;
-  //     document.querySelector(`.score`).textContent = 0;
-  //   }
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector(`.message`).textContent = ``;
-  //     score--;
-  //     document.querySelector(`.score`).textContent = score;
-  //   } else {
-  //     document.querySelector(`.message`).textContent = `you lost the game 😞`;
-  //     document.querySelector(`.score`).textContent = 0;
-  //     document.querySelector(`body`).style.backgroundColor = `#60b347`;
-  //   }
-  // }
+  } 
 });
 
 document.querySelector(`.again`).addEventListener(`click`, function () {
